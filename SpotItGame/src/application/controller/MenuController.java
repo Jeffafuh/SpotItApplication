d87e2ee77e5f8c5ab70cad525e39569b8b1d40bd
Shi-Dev <ponnalavamshi08@gmail.com>
2021-11-17 16:57:03 -0600

    Menu Screen update 1.1
    Added the Menu Screen

--- SpotItGame/src/application/controller/MenuController.java
+++ SpotItGame/src/application/controller/MenuController.java
@@ -1,5 +1,23 @@
 package application.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class MenuController {
+
+    @FXML
+    void switchLeaderboards(ActionEvent event) {
+
+    }
+
+    @FXML
+    void switchToCreate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void switchToPlay(ActionEvent event) {
+
+    }
 
 }
