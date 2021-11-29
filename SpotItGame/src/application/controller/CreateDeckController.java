12b65756548612ab53a7acf937e9494bc3d813ff
Jeff Dong <85254367+Jeffafuh@users.noreply.github.com>
2021-11-28 19:11:28 -0600

    Update CreateDeckController.java

--- SpotItGame/src/application/controller/CreateDeckController.java
+++ SpotItGame/src/application/controller/CreateDeckController.java
@@ -16,7 +16,6 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
